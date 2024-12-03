d21b73d1e61be786cd8b114519bce3365f43aa42
giuseppecasaburi <enzobiondi0000@libero.it>
2024-12-03 16:55:39 +0100

    feat: hobbies

---
+++ hobbies/hobbies.js
@@ -0,0 +1,7 @@
+function hobbies (firstHobbies, secondHobbies, thirdHobbies) {
+    return {
+        hobbies: [firstHobbies, secondHobbies, thirdHobbies]
+    };
+};
+
+console.log(hobbies("calcio", "basket", "videogiochi"));
